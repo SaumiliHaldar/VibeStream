7bb2b96800c5ef9ec7af4be6982cb96150aa9a91
SAUMILI HALDAR <138258411+SaumiliHaldar@users.noreply.github.com>
2025-02-04 16:26:06 +0530

    build.sh modified

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
-# !/user/bin/env bash
+#!/usr/bin/env bash
 # exit on error
 set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
 
-python mange.py collectstatic --no-input
-python manage.py migrate
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Run migrations
+python manage.py migrate --no-input
